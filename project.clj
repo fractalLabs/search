5a987942ae47c7708b031ac4c3712bdf54b7a5d0
nex <andres@fractalmedia.mx>
2011-11-09 07:34:02 -0600

    preparado para mandar a github

--- project.clj
+++ project.clj
@@ -4,6 +4,4 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [fm.namespaces/fm.namespaces "1.0.0-SNAPSHOT"]
                  [cj "1.0.0-SNAPSHOT"]]
-  :dev-dependencies[[org.clojars.autre/lein-vimclojure "1.0.0"]]
-   :repositories {"fractal" {:url "http://67.205.67.146:8099/archiva/repository/fractal"}
-                  "snapshots" {:url "http://67.205.67.146:8099/archiva/repository/snapshots"}})
+  :dev-dependencies[[org.clojars.autre/lein-vimclojure "1.0.0"]])
